2a0327f803f9c095a09bb7db7dc3cf06df8bef5d
erlenthi <51139837+erlenthi@users.noreply.github.com>
2026-01-12 20:28:58 +0100

    Refactor error handling in Workflow1 and Process1 methods to improve clarity and specificity of exception messages

--- part-7/errorHandlingChallenge.cs
+++ part-7/errorHandlingChallenge.cs
@@ -1,10 +1,21 @@
 using System.Data;
 using System.Diagnostics;
 
+/*
+This code shows how try catch blocks work. 
+The catch in Workflow interrupts the 
+
+*/
+
+
 string[][] userEnteredValues = new string[][]
 {
             new string[] { "1", "2", "3"},
             new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"},
+            new string[] { "0", "1", "2"},
+            new string[] { "0", "1", "2"},
+            new string[] { "0", "1", "2"},
             new string[] { "0", "1", "2"}
 };
 
@@ -16,7 +27,7 @@ try
     Workflow1(userEnteredValues);
 
 }
-catch (Exception e)
+catch (DivideByZeroException e)
 {
     Console.WriteLine("An error occurred during 'Workflow1'.");
     Console.WriteLine(e.Message);
@@ -35,27 +46,27 @@ static void Workflow1(string[][] userEnteredValues)
 {
 
 
-    try
-    {
-        foreach (string[] userEntries in userEnteredValues)
-        {
 
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
             Process1(userEntries);
 
             Console.WriteLine("'Process1' completed successfully.");
             Console.WriteLine();
-
-
-
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine("'Workflow1' encountered an issue, process aborted.");
+            Console.WriteLine($"Exception type: {e.Message}");
+            Console.WriteLine();
         }
 
+
     }
-    catch (Exception e)
-    {
-        Console.WriteLine("'Workflow1' encountered an issue, process aborted.");
-        Console.WriteLine($"Exception type: {e.Message}");
-        Console.WriteLine();
-    }
+
+
 
 
 }
@@ -69,9 +80,8 @@ static void Process1(String[] userEntries)
     foreach (string userValue in userEntries)
     {
 
-        try
-        {
-            
+    
+
             if (!int.TryParse(userValue, out valueEntered)) throw new FormatException("Expected numbers");
             if (valueEntered == 0) throw new DivideByZeroException("Value can't be 0");
 
@@ -79,12 +89,8 @@ static void Process1(String[] userEntries)
             {
                 int calculatedValue = 4 / valueEntered;
             }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"Exception: {e.Message}");
-            throw;
-        } 
+        
+       
     }
 
 }
