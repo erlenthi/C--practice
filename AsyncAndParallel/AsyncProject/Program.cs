f88582096fb1f24aa59eddca91e2105fce202bb9
erlenthi <51139837+erlenthi@users.noreply.github.com>
2026-01-19 15:14:20 +0100

    Added Async Project for microsoft learn module "Implement asynchronous tasks"

---
+++ AsyncAndParallel/AsyncProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        string filePath = "example.txt";
+        string content = await ReadFileAsync(filePath);
+        Console.WriteLine(content);
+    }
+
+    public static async Task<string> ReadFileAsync(string filePath)
+    {
+       
+ 
+            return await File.ReadAllTextAsync(filePath);
+        
+        //The code below shows the microsoft learn original Streamreader that I replaced
+        // using (StreamReader reader = new StreamReader(filePath))
+        // {   
+        //     string content = await reader.ReadToEndAsync();
+        //     return content;
+        // }
+        
+    }
+
+}
